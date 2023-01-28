9f8bb61d6756908d3ff36e47a5c75f836cd7997d
eddybank <ehorbinski@gmail.com>
2023-01-28 05:58:49 -0500

    iframe modal CSS
    iframe page CSS
    
    CSS for modal component
    
    changes to iframe component

--- src/app/projects/s-iframe/s-iframe.component.ts
+++ src/app/projects/s-iframe/s-iframe.component.ts
@@ -1,35 +1,9 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'solitaire-iframe',
+  standalone: true,
   templateUrl: './s-iframe.component.html',
   styleUrls: ['./s-iframe.component.css']
 })
-export class SIFrameComponent {
-
-  displayFrame() {
-    utilDisplay.display();
-  }
-  
-  hideFrame() {
-    utilHide.hide();
-  }
-
-@ViewChild('myModal', { static: false }) myModal: ElementRef;
-elm: HTMLElement;
-ngAfterViewInit(): void {
-   this.elm = this.myModal.nativeElement as HTMLElement;
-}
-close(): void {
-    this.elm.classList.remove('show');
-    setTimeout(() => {
-      this.elm.style.width = '0';
-    }, 75);
-}
-open(): void {
-    this.elm.classList.add('show');
-    this.elm.style.width = '100vw';
-    utilDisplay.display();
-}
-  
-}
+export class SIFrameComponent {}
